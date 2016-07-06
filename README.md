be38a7a7e5d0674ffd6f13974bc919dea1d75984
machado-r <rodrigo.krv@gmail.com>
2016-07-06 14:50:38 -0300

    README added

---
+++ README.md
@@ -0,0 +1,12 @@
+en-pt_br
+
+##Implementation of basic sorting algorithms using TypeScript
+
+**Usage**
+*Compile .ts file to .js using tsc and run the .js with node.
+
+
+##Implementação de algortimos básicos de ordenação usando TypeScript
+
+**Uso**
+*Compile o arquivo .ts para gerar o .js, rode o rode .js usando node.
---
+++ bubbleSort.js
@@ -0,0 +1,30 @@
+/* Simple implementation of Bubble sort algorithm using TypeScript through iterating loops.
+ * July 2016
+ */
+//set the size of the array
+var range = 10;
+//array to be sorted
+var vet = new Array(range);
+//generates random int from an interval
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+var i;
+var j;
+var x; //for swap
+//populating the array with unsorted elements
+for (i = 0; i < range; i++) {
+    vet[i] = randomIntFromInterval(0, 99);
+}
+console.log(vet);
+//sorting the array
+for (i = 0; i < range; i++) {
+    for (j = 0; j < range - i; j++) {
+        if (vet[j] >= vet[j + 1]) {
+            x = vet[j];
+            vet[j] = vet[j + 1];
+            vet[j + 1] = x;
+        }
+    }
+}
+console.log(vet);
